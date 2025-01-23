df1c5c1bad6489ac8c3f911b5ad5abb531d022a5
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-01-23 13:19:38 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -45,3 +45,44 @@ class Solution {
         
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int idx =0;
+    Map<Integer, Integer> m = new HashMap<Integer, Integer>();
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i=0;i<inorder.length;i++) {
+            m.put(inorder[i], i);
+        }
+        TreeNode result = createTree(preorder, 0, preorder.length-1);
+        return result;
+   
+    }
+    private TreeNode createTree(int[] preorder, int start, int end) {
+
+        if (start > end) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[idx]);
+        idx++;
+        int rootindex = m.get(root.val);
+        root.left = createTree(preorder, start, rootindex-1);
+        root.right = createTree(preorder, rootindex+1, end);
+        return root;
+    }
+}
